861192378dfa4165f2d5e5007be74e55c9cab5c3
Alex  Manalili-Simeon <alex.manalili-simeon@siemens.com>
2026-05-08 14:30:37 -0700

    olivia plz help

--- discogs.py
+++ discogs.py
@@ -5,18 +5,34 @@ discogs.py
 
 this file just interacts with the discogs api
 rate limit is 25 per min for unauthorized
+
+take in the input.csv 
+output track list with time length
+
 """
 
-import requests
+import requests, csv
 
 from api_keys import headers, discogs_url
 
 
+with open('input.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        print(row['album'], row['artist'])
 
-search_example=discogs_url+'database/search?release_title=nevermind&artist=nirvana&per_page=3&page=1'
+        search_str='https://api.discogs.com/database/search?release_title=revolver&artist=beatles&per_page=1&page=1'
 
-response = requests.get(search_example,headers=headers)
-print(response.json())
+        print(search_str)
+
+        response = requests.get(search_str,headers=headers)
+        print(response.json())
+
+
+# search_example=discogs_url+'database/search?release_title=nevermind&artist=nirvana&per_page=3&page=1'
+# 
+# response = requests.get(search_example,headers=headers)
+# print(response.json())
 
 
 

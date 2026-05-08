1f9c2b4e5fbb4da633d5e3e6febb675b5c48b124
Olivia B. Herald <andrewjherald@Olivias-MacBook-Pro.local>
2026-05-08 16:19:44 -0700

    added stuff to find tracklist, genre, style

--- discogs.py
+++ discogs.py
@@ -19,17 +19,32 @@ from api_keys import headers, discogs_url
 with open('input.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        print(row['album'], row['artist'])
+        # print(row['album'], row['artist'])
         # this search string works yay keeeeeep it
         # search_str='https://api.discogs.com/database/search?release_title=' +row['album'] +'&artist=' +row['artist'] +'&per_page=1&page=1'
-        search_str='https://api.discogs.com/database/search?release_title=' +row['album'] +'&artist=' +row['artist'] +'&per_page=1&page=1'
+        search_str='https://api.discogs.com/database/search?release_title=' +row['album'] +'&artist=' +row['artist'] +'&format=Vinyl&per_page=1&page=1'
 
-        print(search_str)
+        # print(search_str)
 
         response = requests.get(search_str,headers=headers)
-        print(response.json())
+        # print(response.json())
+        search_dict=response.json()
 
+        id = str(search_dict["results"][0]["id"])
+        print(id)
 
+        release_str='https://api.discogs.com/releases/'+id
+
+        response = requests.get(release_str,headers=headers)
+        release_dict=response.json()
+        tracklist=release_dict['tracklist']
+        genres=release_dict['genres']
+        styles=release_dict['styles']
+        print(genres,styles) 
+        for track in tracklist:
+            print(track['position'])
+            print(track['title'])
+            print(track['duration'])
 # search_example=discogs_url+'database/search?release_title=nevermind&artist=nirvana&per_page=3&page=1'
 # 
 # response = requests.get(search_example,headers=headers)

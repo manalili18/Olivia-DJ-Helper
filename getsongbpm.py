8b3d07ed8d3ac802ee107b0232056da82a04ec36
Alex  Manalili-Simeon <alex.manalili-simeon@siemens.com>
2026-05-08 10:41:04 -0700

    working example for getsongbpm

---
+++ getsongbpm.py
@@ -0,0 +1,18 @@
+#!/bin/env python3
+
+"""
+main.py
+This is the main file for executing the DJ helper
+"""
+
+import requests
+
+from api_keys import gsb_url, gsb_key
+
+search_example=gsb_url+'search/?api_key='+gsb_key+'&type=artist&lookup=metallica'
+
+print(search_example)
+
+response = requests.get(search_example)
+print(response.json())
+
